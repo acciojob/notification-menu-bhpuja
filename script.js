184ddc7eeaad4f3962436513bcb52555690c0a80
bhpuja <vpuja0421@gmail.com>
2025-01-16 15:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// script.js
+
+// Adding functionality to the "View More" button
+const button = document.getElementById('viewMoreBtn');
+
+button.addEventListener('click', () => {
+  alert('You clicked on "View More". This could navigate to another page or load more notifications!');
+});
